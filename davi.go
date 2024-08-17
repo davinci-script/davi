64f306a8315a16e6572102bc5c1e9e3e1d067d14
Bozhidar <bobi@microweber.com>
2024-08-17 16:31:07 +0300

    update

--- davi.go
+++ davi.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	. "github.com/DavinciScript/Davi/lexer"
 )
 
 func main() {
@@ -9,13 +11,20 @@ func main() {
 	fmt.Println("Running davi.go")
 
 	// Run the lexer
-	lexer := NewLexer([]byte(`print(1234, "foo") @`))
+	lexer := NewLexer([]byte(`print(1234, "foo")`))
 	for {
 		pos, tok, val := lexer.Next()
 		if tok == EOF {
 			break
 		}
-		fmt.Printf("%d:%d %s %q\n", pos.Line, pos.Column, tok, val)
+		data := map[string]interface{}{
+			"posLine":   pos.Line,
+			"posColumn": pos.Column,
+			"token":     tok,
+			"value":     val,
+		}
+		out, _ := json.MarshalIndent(data, "", "  ")
+		fmt.Println(string(out))
 		if tok == ILLEGAL {
 			break
 		}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DavinciScript/Davi
 
 go 1.21.5
+
+require github.com/lmittmann/tint v1.0.5 // indirect
